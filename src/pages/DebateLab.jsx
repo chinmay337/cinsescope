e93af60f70b440309d31dfe3d398e0bc1b77cc2c
Chinmay <chinmay0337@gmail.com>
2026-07-01 19:07:49 +0530

    Debate Lab Page Structure completed

--- src/pages/DebateLab.jsx
+++ src/pages/DebateLab.jsx
@@ -1,5 +1,57 @@
 function DebateLab() {
-  return <h1>Debate Lab</h1>;
+  return (
+    <div className="min-h-screen bg-gray-950 text-white p-8">
+
+      <div className="max-w-6xl mx-auto">
+
+        <h1 className="text-5xl font-bold text-center">
+          ⚔️ Debate Lab
+        </h1>
+
+        <p className="text-center text-gray-400 mt-3">
+          Settle the ultimate movie debate.
+        </p>
+
+      </div>
+      <div className="flex items-center justify-center gap-6 mt-12">
+
+        <input
+          type="text"
+          placeholder="🔍 Search first movie..."
+          className="w-96 bg-gray-900 border border-gray-800 rounded-xl p-4 outline-none"
+        />
+
+        <div className="text-2xl font-bold">
+          VS
+        </div>
+
+        <input
+          type="text"
+          placeholder="🔍 Search second movie..."
+          className="w-96 bg-gray-900 border border-gray-800 rounded-xl p-4 outline-none"
+        />
+
+      </div>
+      <div className="bg-gray-900 rounded-2xl mt-12 h-72 flex items-center justify-center text-gray-500">
+
+        Movie Cards will appear here
+
+      </div>
+
+      <div className="bg-gray-900 rounded-2xl mt-8 h-64 flex items-center justify-center text-gray-500">
+
+        Comparison Table
+
+      </div>
+
+      <div className="bg-gray-900 rounded-2xl mt-8 h-40 flex items-center justify-center text-gray-500">
+
+        🎬 CineScope Verdict
+
+      </div>
+      
+    </div>
+  );
 }
 
 export default DebateLab;
--- src/services/gemini.js
+++ src/services/gemini.js
@@ -7,22 +7,15 @@ const ai = new GoogleGenAI({
 export async function analyzeMood(userMood) {
     const prompt = `
 You are CineScope AI.
-
 Your personality is calm, cinematic, thoughtful and slightly poetic.
-
 The user will describe the kind of movie experience they want.
-
 Return ONLY valid JSON.
-
-Format:
-
+Example Format:
 {
   "genre":"Drama",
   "insight":"Some stories don't fix bad days... they simply make them feel a little lighter."
 }
-
 Rules:
-
 - Choose ONLY one genre from this list:
 Action
 Adventure
@@ -36,14 +29,12 @@ Horror
 Romance
 Science Fiction
 Thriller
-
 - The insight must feel human, warm and cinematic.
 - Maximum 20 words.
 - Never sound robotic.
 - Never explain your reasoning.
 - Never use markdown.
 - Return ONLY JSON.
-
 User request:
 ${userMood}
 `;
